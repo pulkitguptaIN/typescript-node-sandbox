5e9af753c3d158d4ecfcae870650265b444dae76
Pulkit Gupta <pulgupta2@publicisgroupe.net>
2020-07-17 16:49:47 +0530

    added method in string-utils for string interpolation

--- src/util/string-util.ts
+++ src/util/string-util.ts
@@ -1,7 +1,28 @@
 export class StringUtil {
 
-    sayHello(name: string): string {
-        return `Hello ${name}`;
+    /**
+     * evaluates given inputStr by resolving placeholders using given placeholderKeyValueMap
+     * 
+     * @param inputStr input string with placeholder enclosed within ${placeholderKey}
+     * @param placeholderKeyValueMap map of placeholder keys and its replacement values
+     */
+    evalPlaceholders(inputStr: string, placeholderKeyValueMap: Map<string, string>): string {
+        const placeholderRegex = /\${(.*?)}/g;
+        /* initialy copy inputStr to processedStr as-is*/
+        let processedStr: string = inputStr;
+        /* process iff inputStr and placeholderKeyValueMap is not empty*/
+        if (inputStr && placeholderKeyValueMap && placeholderKeyValueMap.size > 0) {
+            processedStr = inputStr.replace(placeholderRegex, (placeholderStr, placeholderKey) => {
+                /* lookup placeholderKey in placeholderKeyValueMap*/
+                let replacementStr = placeholderKeyValueMap.get(placeholderKey);
+                if (replacementStr === undefined) {
+                    /* use placeholderStr as-is if vaid replacement is not found in placeholderKeyValueMap*/
+                    replacementStr = placeholderStr;
+                }
+                return replacementStr;
+            });
+        }
+        return processedStr;
     }
-    
+
 }
--- test/util/string-util.test.ts
+++ test/util/string-util.test.ts
@@ -1,10 +1,28 @@
 import { StringUtil } from '../../src/util/string-util';
+
 describe('StringUtil Test', () => {
 
     const stringUtil: StringUtil = new StringUtil();
 
-    test('testSayHello', () => {
-        expect(stringUtil.sayHello('Pulkit')).toEqual('Hello Pulkit');
+    test('testEvalPlaceholders', () => {
+        const placeholderKeyValueMap: Map<string, string> = new Map<string, string>();
+        placeholderKeyValueMap.set('firstName', 'Pulkit');
+        placeholderKeyValueMap.set('lastName', 'Gupta');
+        placeholderKeyValueMap.set('currentCity', 'Gurgaon');
+        placeholderKeyValueMap.set('currentAge', '30');
+        const inputStr = 'My name is ${firstName}, ${lastName}, I live in ${currentCity} and I am ${currentAge} years old';
+        // regular flow, all keys present in map thus resolving all placeholders
+        expect(stringUtil.evalPlaceholders(inputStr, placeholderKeyValueMap)).toMatch('My name is Pulkit, Gupta, I live in Gurgaon and I am 30 years old');
+
+        placeholderKeyValueMap.set('lastName', '');
+        placeholderKeyValueMap.delete('currentAge');
+        // partial flow, few keys are missing in map thus resolving only placeholders whose key is found
+        expect(stringUtil.evalPlaceholders(inputStr, placeholderKeyValueMap)).toMatch('My name is Pulkit, , I live in Gurgaon and I am ${currentAge} years old');
+
+        //invaid flow, input is invalid thus returning input as-is without resolving anything
+        expect(stringUtil.evalPlaceholders(inputStr, new Map<string, string>())).toMatch(inputStr);
+        expect(stringUtil.evalPlaceholders('', placeholderKeyValueMap)).toMatch('');
+        expect(stringUtil.evalPlaceholders('This is test string', placeholderKeyValueMap)).toMatch('This is test string');
     });
 
 });
